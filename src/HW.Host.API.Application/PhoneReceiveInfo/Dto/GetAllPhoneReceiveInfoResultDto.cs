06834455222e9ad29f85504715ba691b32fbe9e1
Mr.Fang <2875616188@qq.com>
2020-12-07 14:20:31 +0800

    添加手机领取信息服务

---
+++ src/HW.Host.API.Application/PhoneReceiveInfo/Dto/GetAllPhoneReceiveInfoResultDto.cs
@@ -0,0 +1,64 @@
+﻿using HW.Host.API.Infrastructure.Model;
+using HW.Host.API.Model.ModelEntity;
+using System;
+using System.Collections.Generic;
+
+namespace HW.Host.API.Application.PhoneReceiveInfo.Dto
+{
+    /// <summary>
+    /// 得到所有手机领取详情返回类
+    /// </summary>
+    public class GetAllPhoneReceiveInfoResultDto : ResultDto
+    {
+        /// <summary>
+        /// 手机领取信息集合
+        /// </summary>
+        public List<HW_PhoneReceiveInfo> PhoneReceiveInfoList { get; set; }
+
+        /// <summary>
+        /// 手机领取信息
+        /// </summary>
+        public class PhoneReceiveInfo
+        {
+            /// <summary>
+            /// Id
+            /// </summary>
+            public int Id { get; set; }
+
+            /// <summary>
+            /// 领取时间
+            /// </summary>
+            public DateTime ReceiveTime { get; set; } = DateTime.Now;
+
+            /// <summary>
+            /// 领取员工ID
+            /// </summary>
+            public int ReceiveUserID { get; set; }
+
+            /// <summary>
+            /// 领取型号详情ID
+            /// </summary>
+            public int ReceivePhoneModelID { get; set; }
+
+            /// <summary>
+            /// 是否归还
+            /// </summary>
+            public int ReceiveIsReturn { get; set; } = 0;
+
+            /// <summary>
+            /// 归还时间
+            /// </summary>
+            public DateTime ReceiveReturnTime { get; set; }
+
+            /// <summary>
+            /// 领取备注
+            /// </summary>
+            public string ReceiveRemarks { get; set; }
+
+            /// <summary>
+            /// 归还备注
+            /// </summary>
+            public string ReturnRemarks { get; set; }
+        }
+    }
+}
---
+++ src/HW.Host.API.Application/PhoneReceiveInfo/PhoneReceiveInfoService.cs
@@ -0,0 +1,36 @@
+﻿using HW.Host.API.Application.PhoneReceiveInfo.Dto;
+using HW.Host.API.Infrastructure.SqlSugar;
+using HW.Host.API.Model.ModelEntity;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace HW.Host.API.Application.PhoneReceiveInfo
+{
+    /// <summary>
+    /// 手机领取服务
+    /// </summary>
+    [ApiExplorerSettings(GroupName = "HW.Host.App.API")]
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class PhoneReceiveInfoService
+    {
+        private readonly IOwnerRepository<HW_PhoneReceiveInfo> _context;
+
+        public PhoneReceiveInfoService(IOwnerRepository<HW_PhoneReceiveInfo> context)
+        {
+            _context = context;
+        }
+
+        /// <summary>
+        /// 得到所有手机领取详情信息
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost("GetAllPhoneReceiveInfo")]
+        public async Task<GetAllPhoneReceiveInfoResultDto> GetAllPhoneReceiveInfo()
+        {
+            return new GetAllPhoneReceiveInfoResultDto();
+        }
+    }
+}
